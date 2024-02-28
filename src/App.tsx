f70805dd3bb28b305125396948912794f782263c
Matsuyama01 <matu07100710@gmail.com>
2024-02-28 21:50:57 +0900

    大体のひな型を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <div className='App'></div>;
-}
+import React, { useState } from 'react';
+
+const App: React.FC = () => {
+  const [list, setList] = useState([]);
+  // テキスト入力時の処理
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
+  // 追加ボタンを押したときの処理
+  const onClick = () => {};
+
+  return (
+    <div className='App'>
+      <div>
+        <h2>Todoリスト</h2>
+        <button onClick={}>追加</button>
+        <input type='text' onChange={handleChange} className='inputtext' />
+        <ul>
+          リスト
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default App;
