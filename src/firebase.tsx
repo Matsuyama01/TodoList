ff7f94b85dbd5da299301fd2afb0f31598a34a13
Matsuyama01 <matu07100710@gmail.com>
2024-03-22 12:16:46 +0900

    APIを直接書いていたのでenvに書き直し

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBaYBKqb-Fa-bCGWNeEChuQr4ZNuaBLt7k',
-  authDomain: 'todolisit-ea7e7.firebaseapp.com',
-  projectId: 'todolisit-ea7e7',
-  storageBucket: 'todolisit-ea7e7.appspot.com',
-  messagingSenderId: '275368996288',
-  appId: '1:275368996288:web:06b38f0f6f5410838e2c25',
-  measurementId: 'G-P1B3MFC99V',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
