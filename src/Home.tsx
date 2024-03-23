5c942c0ae82104233ce03181e413940f8d627dfb
Matsuyama01 <matu07100710@gmail.com>
2024-03-23 18:55:14 +0900

    型を追加、処理部分を移動

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,9 +2,41 @@ import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import TodoList from './components/TodoList';
+import React from 'react';
 
-function Home() {
+const Home: React.FC = () => {
   const [user] = useAuthState(auth);
+
+  // Googleでサインイン
+  function SignInButton() {
+    const signInWithGoogle = () => {
+      //firebaseを使ってサインイン
+      signInWithPopup(auth, provider);
+    };
+
+    return (
+      <button onClick={signInWithGoogle}>
+        <p>Googleでサインイン</p>
+      </button>
+    );
+  }
+
+  // Googleでサインアウト
+  function SignOutButton() {
+    return (
+      <button onClick={() => auth.signOut()}>
+        <p>サインアウト</p>
+      </button>
+    );
+  }
+
+  function UserInfo() {
+    // photoURLが存在する場合のみ<img>に渡し、そうでなければ<img>をレンダリングしない
+    const photoURL = auth.currentUser?.photoURL as string;
+    return (
+      <div>{photoURL ? <img src={photoURL} alt='User profile' /> : null}</div>
+    );
+  }
 
   return (
     <div>
@@ -19,37 +51,6 @@ function Home() {
       )}
     </div>
   );
-}
+};
 
 export default Home;
-
-// Googleでサインイン
-function SignInButton() {
-  const signInWithGoogle = () => {
-    //firebaseを使ってサインイン
-    signInWithPopup(auth, provider);
-  };
-
-  return (
-    <button onClick={signInWithGoogle}>
-      <p>Googleでサインイン</p>
-    </button>
-  );
-}
-
-// Googleでサインアウト
-function SignOutButton() {
-  return (
-    <button onClick={() => auth.signOut()}>
-      <p>サインアウト</p>
-    </button>
-  );
-}
-
-function UserInfo() {
-  // photoURLが存在する場合のみ<img>に渡し、そうでなければ<img>をレンダリングしない
-  const photoURL = auth.currentUser?.photoURL as string;
-  return (
-    <div>{photoURL ? <img src={photoURL} alt='User profile' /> : null}</div>
-  );
-}
